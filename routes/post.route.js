ac7facea4cfc04d5230a6528efe3066cf2f54f69
ryu820 <rhju820@naver.com>
2023-03-24 19:26:24 +0900

    [fix]post fix

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { Posts } = require("../models")
+const { Posts ,Likes } = require("../models")
 const router = express.Router()
 
 //게시글 작성
@@ -13,16 +13,21 @@ router.post("/", async (req, res) => {
 //게시글 전체 조회
 //localhost:3000/posts GET
 router.get("/", async (req, res) => {
-
+    const {userId} = res.locals.user
     const posts = await Posts.findAll({
         raw: true,
-        attributes: ['postId', 'User.account', "User.nickname", "img", "content", "likeCount", "createdAt", "updatedAt"],
+        attributes: ['postId', "User.nickname", "img", "content", "likeCount", "createdAt", "updatedAt"],
         order: [['postId', 'DESC']],
         include: [{
             model: Users,
             attributes: []
         }]
     })
+    const likes = await Likes.findAll({
+        where : {userId},
+        attributes: ["isLikes"],
+    })
+    if (likes)
     res.status(200).json({ posts })
 })
 
