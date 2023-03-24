de4b1b2728dd49476d5554cc40fc4014c8fd24cd
ryu820 <rhju820@naver.com>
2023-03-24 16:50:41 +0900

    [fix]게시글 작성 및 조회 기본세팅

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,5 +1,29 @@
 const express = require('express')
+const { Posts } = require("../models")
 const router = express.Router()
 
-console.log("test")
+//게시글 작성
+//localhost:3000/posts POST
+router.post("/", async (req, res) => {
+    const { img, comtent } = req.body;
+    await Posts.create({ img, comtent })
+    res.status(200).json({ "message": "게시글 작성에 성공하였습니다." })
+})
+
+//게시글 전체 조회
+//localhost:3000/posts GET
+router.get("/", async (req, res) => {
+
+    const posts = await Posts.findAll({
+        raw: true,
+        attributes: ['postId', 'User.account', "User.nickname", "img", "content", "likeCount", "createdAt", "updatedAt"],
+        order: [['postId', 'DESC']],
+        include: [{
+            model: Users,
+            attributes: []
+        }]
+    })
+    res.status(200).json({ posts })
+})
+
 module.exports = router;
