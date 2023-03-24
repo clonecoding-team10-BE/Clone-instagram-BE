77cf021886400c681016bc21db88dd58848c6665
ryu820 <rhju820@naver.com>
2023-03-25 01:03:47 +0900

    [fix]게시글 전체 조회 수정 업데이트

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,34 +1,66 @@
 const express = require('express')
-const { Posts ,Likes } = require("../models")
+const { Posts, Likes, Users,Comments } = require("../models")
 const router = express.Router()
 
 //게시글 작성
 //localhost:3000/posts POST
 router.post("/", async (req, res) => {
-    const { img, comtent } = req.body;
-    await Posts.create({ img, comtent })
+    const { img, content, userId } = req.body;
+    await Posts.create({
+        userId, //나중에 삭제
+        img,
+        content
+    })
     res.status(200).json({ "message": "게시글 작성에 성공하였습니다." })
 })
 
 //게시글 전체 조회
 //localhost:3000/posts GET
 router.get("/", async (req, res) => {
-    const {userId} = res.locals.user
+    // const { userId } = res.locals.user
+    const { userId } = req.body;
     const posts = await Posts.findAll({
         raw: true,
-        attributes: ['postId', "User.nickname", "img", "content", "likeCount", "createdAt", "updatedAt"],
-        order: [['postId', 'DESC']],
+        attributes: ["postId", "User.nickname", "img", "content", "likeCount", "Likes.isLike", "createdAt", "updatedAt"],
+        order: [['postId', 'DESC']], //최신순 정렬
         include: [{
             model: Users,
             attributes: []
+        }, {
+            model: Likes,
+            attributes: []
         }]
     })
-    const likes = await Likes.findAll({
-        where : {userId},
-        attributes: ["isLikes"],
-    })
-    if (likes)
-    res.status(200).json({ posts })
+    //isLike가 존재하지않을때 -> 좋아요를 누르지 않았을 때
+    //isLike 값을 강제로 false 반환
+    const postList = await Promise.all(posts.map(async (post) => {
+        const postId = post.postId
+        //postId에 해당하는 comment도 같이 response
+        const comments = await Comments.findAll({
+            where : {postId},
+            order: [['commentId', 'DESC']], //최신순 정렬
+        })
+
+        if (post.isLike === null) {
+            post.isLike = false
+        } else {
+            post.isLike = true
+        }
+        // console.log(post.isLike)
+        return {
+            "postId": post.postId,
+            "nickname": post.nickname,
+            "img": post.img,
+            "content": post.content,
+            "likeCount": post.likeCount,
+            "isLike": post.isLike,
+            "createdAt": post.createdAt,
+            "updatedAt": post.updatedAt,
+            "comment" : comments
+        }
+    }))
+
+    res.status(200).json({ postList })
 })
 
 module.exports = router;
