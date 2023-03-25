74ba3f75f78c9ca9365390aed38db79c835130fd
ryu820 <rhju820@naver.com>
2023-03-25 14:26:03 +0900

    [fix]전체 코드리뷰 및 수정 업데이트

--- routes/login.route.js
+++ routes/login.route.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 const { Users } = require('../models');
-const authMiddleware = require('../middleware/auth');
+const env = process.env
 
 
 router.post(
@@ -14,35 +14,31 @@ router.post(
     check('password', '비밀번호가 필요합니다').exists(),
   ],
   async (req, res) => {
- 
+    //req값 validation 체크 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
     const { email, password } = req.body;
-
     try {
-      // 만약 유저가 존재한다면
+      // 만약 유저가 존재하지 않는다면
       const user = await Users.findOne({ where: { email: email } });
       if (!user) {
-        return res.status(400).json({ errors: [{ msg: '유저가 존재하지 않습니다' }] });
+        return res.status(400).json({ "errorMessage": '유저가 존재하지 않습니다' });
       }
 
-      // 패스워드가 일치한다면
+      // 패스워드가 일치하지 않는다면
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
-        return res.status(400).json({ errors: [{ msg: '비밀번호가 일치하지 않습니다' }] });
+        return res.status(400).json({ "errorMessage": '비밀번호가 일치하지 않습니다' });
       }
 
-      
-      const payload = {
-        user: {
-          id: user.userId,
-        },
-      };
-      
-      
+      const token = jwt.sign({ nickname: user.nickname }, env.MYSQL_SECRETKEY);
+      res.cookie("authorization", `Bearer ${token}`);
+
+      res.status(200).json({ "message": "로그인에 성공하였습니다." })
+
     } catch (err) {
       console.error(err.message);
       res.status(500).send('서버에러가 발생했습니다');
