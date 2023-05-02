b7da5e008fc687b975499854290a4039d65f7aa7
ryu820 <rhju820@naver.com>
2023-05-03 02:26:26 +0900

    [feat]서버 부하테스트

--- services/login.service.js
+++ services/login.service.js
@@ -10,15 +10,16 @@ class LoginService {
 
   remainUser = async ({email, password}) => {
     // Find the user with the given email
+    console.log(email)
     const user = await this.userRepository.getUserByEmail({email});
     if (!user) {
-      throw new CustomError('유저가 존재하지 않습니다', 400);
+      throw new CustomError('유저가 존재하지 않습니다', 401);
     }
 
     // Check if the password is correct
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      throw new CustomError('비밀번호가 일치하지 않습니다', 400);
+      throw new CustomError('비밀번호가 일치하지 않습니다', 401);
     }
 
     return user;
